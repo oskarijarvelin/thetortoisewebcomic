2b36ec8d4b07b454e935ded4ae1547c2ebc40886
Oskari Järvelin <contact@oskarijarvelin.fi>
2022-10-11 00:36:28 +0300

    Updated single comic layout

--- pages/comics/[slug].js
+++ pages/comics/[slug].js
@@ -2,7 +2,12 @@ import { Layout } from "../../components/Layout";
 import { useTina } from "tinacms/dist/edit-state";
 import { TinaMarkdown } from 'tinacms/dist/rich-text';
 import { client } from "../../.tina/__generated__/client";
-import Link from 'next/link';
+import Link from '../../components/Link';
+import { Box } from "@mui/material";
+import { Typography } from "@mui/material";
+import { GrLink } from 'react-icons/gr';
+import Moment from 'react-moment';
+import moment from 'moment';
 
 export default function Home(props) {
 
@@ -14,15 +19,30 @@ export default function Home(props) {
 
   return (
     <Layout>
-      <Link href={data.comics.imgSrc} >
-        <a>
-          <img data-tinafield="imgSrc" src={data.comics.imgSrc} />
-        </a>
-      </Link>
-      <h1 data-tinafield="title">{data.comics.title}</h1>
-      <div data-tinafield="data.comics.body">
-        <TinaMarkdown content={data.comics.body} />
-        </div>
+      <Box>
+
+        <Link href={data.comics.imgSrc}>
+          <a>
+            <img data-tinafield="imgSrc" src={data.comics.imgSrc} width="100%" height="auto" />
+          </a>
+        </Link>
+
+        <Typography variant="h3" component="h1" sx={{ mt: 4, mb: 8 }} data-tinafield="title">
+          <Link href={`/comics/${data.comics._sys.filename}`} color="inherit" sx={{ textDecoration: 'none' }}>
+            <small><GrLink /></small> {data.comics.title}
+          </Link>
+        </Typography>
+
+        <Typography sx={{ fontWeight: 500, color: '#666' }}>
+          <span data-tinafield="index">#{data.comics.index}</span>{' '}&bull;{' '}
+          <span data-tinafield="date"><Moment format="DD.MM.YYYY">{data.comics.date}</Moment></span>
+        </Typography>
+
+        <Box data-tinafield="body" sx={{ mt: 4 }}>
+          <TinaMarkdown content={data.comics.body} />
+        </Box>
+
+      </Box>
     </Layout>
   );
 }
