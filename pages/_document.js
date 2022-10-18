ea87d20bbbdb0dca62a2e22ec9a5ffc91b0ce49b
Oskari Järvelin <contact@oskarijarvelin.fi>
2022-10-19 01:00:45 +0300

    Removed images, added fontsource

--- pages/_document.js
+++ pages/_document.js
@@ -11,11 +11,7 @@ export default class MyDocument extends Document {
         <Head>
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
-          <link rel="shortcut icon" href="/favicon.ico" />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-          />
+          <link rel="shortcut icon" href="/favicon.png" />
           <meta name="emotion-insertion-point" content="" />
           {this.props.emotionStyleTags}
         </Head>
